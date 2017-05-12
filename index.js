468ce747ea3aa8c69c300f8436e54a68e809a5bf
Boris Kubiak <kubiakboris@gmail.com>
2017-05-12 08:15:44 +0200

    Add Google App engine compatibility

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const PORT = process.env.PORT || 80;
+const SSL_PORT = process.env.SSL_PORT || 443;
+
 var App = require("./js/app");
 var app = new App.App();
 // [port, 'hostname'] | [port, '']
-app.start({http: [80, ''], https: [443, '']});
+app.start({http: [PORT, ''], https: [SSL_PORT, '']});
